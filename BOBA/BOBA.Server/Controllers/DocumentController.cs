65024f76a812411b7dfe37a58a295c9fc60e004e
Tarcza Lídia <100163235+diatrcz@users.noreply.github.com>
2025-05-22 08:33:18 +0200

    Document handling

--- BOBA/BOBA.Server/Controllers/DocumentController.cs
+++ BOBA/BOBA.Server/Controllers/DocumentController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BOBA.Server.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+using System.Security.Claims;
 
 namespace BOBA.Server.Controllers
 {
@@ -8,5 +12,42 @@ namespace BOBA.Server.Controllers
     [Authorize]
     public class DocumentController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        private const string starterId = "1";
+
+        public DocumentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+
+                var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                using var ms = new MemoryStream();
+                await file.CopyToAsync(ms);
+                var document = new FormDocument
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    TaskId = "2",
+                    FileName = file.FileName,
+                    ContentType = file.ContentType,
+                    Data = ms.ToArray(),
+                    UploadeddAt = DateTime.UtcNow,
+                    UploaderId = userId
+                };
+
+                _context.FormDocuments.Add(document);
+                await _context.SaveChangesAsync();
+
+                return Ok("Uploaded");
+            }
+
+            return BadRequest("Invalid file");
+        }
+
     }
 }
