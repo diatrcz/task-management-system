4d98db31c0c21b083aa9095d785242fdf62cb773
Tarcza Lídia <100163235+diatrcz@users.noreply.github.com>
2025-11-24 23:10:56 +0100

    feat: add registration logic to server side

---
+++ BOBA/BOBA.Server/Models/RegisterResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BOBA.Server.Models;
+
+public class RegisterResponse
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public IEnumerable<string>? Errors { get; set; }
+}
---
+++ BOBA/BOBA.Server/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BOBA.Server.Models;
+
+public class UserModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    public string[] TeamIds { get; set; } = Array.Empty<string>();
+}
+
